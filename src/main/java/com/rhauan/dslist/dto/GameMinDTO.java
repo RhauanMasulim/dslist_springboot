ceb4515ceb84499339203d3431187f3293d382a9
Rhauan Masulim <rmasulim@gmail.com>
2023-07-20 21:24:46 -0300

    SQL, projection, get games by list

--- src/main/java/com/rhauan/dslist/dto/GameMinDTO.java
+++ src/main/java/com/rhauan/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.rhauan.dslist.dto;
 
 import com.rhauan.dslist.entities.Game;
+import com.rhauan.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -20,6 +21,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
